60e61942b54a84395204dfb2c9e757283fe7dec4
Giap Chin <awesome.giap@gmail.com>
2021-01-30 19:41:05 +0800

    v0.2.9: Made a mixin to allow per-component usage

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 This is a fork from [Vue Container Query 2](https://github.com/Mehdi-Hp/vue-container-query-2) with a bug fix when 'useBEM' is set to false.
 
 This library does not support Vue 3 (yet).
+
+## v0.2.9:
+- Now exports a new function ```VueContainerQueryMixin```, to allow the use of Vue mixin for per-component usage instead of enabling CQ globally.
+    - The ```VueContainerQueryMixin``` function returns an object which can be used as mixin. This function accepts a config object. E.g.:
+
+```js
+import { VueContainerQueryMixin } from 'vue-container-query-3';
+
+const cqMixin = VueContainerQueryMixin({
+	classNames: {
+		sizes: {
+			'sm': 'sm',
+			'md': 'md',
+			'lg': 'lg',
+			'xl': 'xl',
+			'2xl': '2xl',
+		},
+		prepend: 'cq-',
+	},
+	useBEM: false,
+});
+
+export default {
+	name: 'DemoComponent',
+	mixins: [
+		cqMixin,
+    ],
+    cq: {
+		'sm': { minWidth: 640 },
+		'md': { minWidth: 768 },
+		'lg': { minWidth: 1024 },
+		'xl': { minWidth: 1280 },
+		'2xl': { minWidth: 1536 },
+	},
+}
+```
+
--- src/cq.js
+++ src/cq.js
@@ -1,111 +1,122 @@
+import Vue from 'vue';
 import ResizeObserver from 'resize-observer-polyfill';
 import ClassCalculator from './classCalculator/index';
 import { optionsUtils } from './_helpers';
 
 const goodToGo = (context) => {
-    return context.$options.cq && !!Object.keys(context.$options.cq).length;
+	return context.$options.cq && !!Object.keys(context.$options.cq).length;
 };
 
 const cssMediaQueryResolver = {
-    maxWidth(desired, actual) {
-        return actual <= desired;
-    },
-    minWidth(desired, actual) {
-        return actual > desired;
-    }
+	maxWidth(desired, actual) {
+		return actual <= desired;
+	},
+	minWidth(desired, actual) {
+		return actual > desired;
+	}
 };
 
 const defaultOptions = {
-    classNames: {
-        sizes: {
-            xsmall: 'xsmall',
-            small: 'small',
-            medium: 'medium',
-            large: 'large',
-            xlarge: 'xlarge'
-        },
-        prepend: ''
-    },
-    useBEM: true,
-    utilityClassNamesRegex: /$^/,
-    ignoredClasses: ['']
+	classNames: {
+		sizes: {
+			xsmall: 'xsmall',
+			small: 'small',
+			medium: 'medium',
+			large: 'large',
+			xlarge: 'xlarge'
+		},
+		prepend: ''
+	},
+	useBEM: true,
+	utilityClassNamesRegex: /$^/,
+	ignoredClasses: [''],
 };
+
+
+const getMixin = (normalizedOptions) => {
+	return {
+		directives: {
+			cq: {
+				bind($el, { modifiers }, vnode) {
+					const classCalculator = new ClassCalculator(vnode.context, vnode.data.staticClass, $el, normalizedOptions);
+					vnode.context.$on('$cq:resize', (contentRect) => {
+						classCalculator.setNewClasses();
+						vnode.context.$forceUpdate();
+					});
+				},
+				unbind($el, { modifiers }, vnode) {
+					vnode.context.$off('$cq:resize');
+				},
+			},
+		},
+		computed: {
+			$cq() {
+				if (goodToGo(this)) {
+					return this._cq;
+				}
+				return null;
+			}
+		},
+		beforeCreate() {
+			if (goodToGo(this)) {
+				this._cq = Vue.observable({
+					contentRect: {},
+					breakpoints: {},
+					resizeObserver: null
+				});
+			}
+		},
+		created() {
+			if (goodToGo(this)) {
+				this.$set(this._cq, 'breakpoints', this.$options.cq);
+			}
+		},
+		mounted() {
+			if (goodToGo(this)) {
+				Object.keys(this._cq.breakpoints).forEach((breakpoint) => {
+					this._cq[breakpoint] = false;
+				});
+
+				this._cq.resizeObserver = new ResizeObserver((entries) => {
+					console.log({ entries });
+					entries.forEach((entry) => {
+						this.$set(this._cq, 'contentRect', entry.contentRect.toJSON());
+						Object.keys(this._cq.breakpoints).forEach((breakpoint) => {
+							const rules = this._cq.breakpoints[breakpoint];
+							Object.entries(rules).forEach((rule) => {
+								const cssMqRule = rule[0];
+								const cssMqValue = rule[1];
+								const fieldToTest = (cssMqRule.toLowerCase().includes('width')) ? 'width' : 'height';
+								this.$set(this._cq, breakpoint, cssMediaQueryResolver[cssMqRule](cssMqValue, this._cq.contentRect[fieldToTest]));
+								this.$emit('$cq:resize', this._cq.contentRect);
+							});
+						});
+					});
+				});
+				this._cq.resizeObserver.observe(this.$el);
+			}
+		},
+		destroyed() {
+			if (goodToGo(this)) {
+				this._cq.resizeObserver.disconnect();
+			}
+		}
+	}
+}
 
 export default {
-    install(Vue, options) {
-        optionsUtils.validate(options, defaultOptions);
-        const normalizedOptions = optionsUtils.normalize(options, defaultOptions);
+	install(Vue, options) {
+		optionsUtils.validate(options, defaultOptions);
+		const normalizedOptions = optionsUtils.normalize(options, defaultOptions);
 
-        Vue.mixin({
-            directives: {
-                cq: {
-                    bind($el, { modifiers }, vnode) {
-                        const classCalculator = new ClassCalculator(vnode.context, vnode.data.staticClass, $el, normalizedOptions);
-                        vnode.context.$on('$cq:resize', (contentRect) => {
-                            classCalculator.setNewClasses();
-                            vnode.context.$forceUpdate();
-                        });
-                    },
-                    unbind($el, { modifiers }, vnode) {
-                        vnode.context.$off('$cq:resize');
-                    }
-                }
-            },
-            data() {
-                return {};
-            },
-            computed: {
-                $cq() {
-                    if (goodToGo(this)) {
-                        return this._cq;
-                    }
-                    return null;
-                }
-            },
-            beforeCreate() {
-                if (goodToGo(this)) {
-                    this._cq = Vue.observable({
-                        contentRect: {},
-                        breakpoints: {},
-                        resizeObserver: null
-                    });
-                }
-            },
-            created() {
-                if (goodToGo(this)) {
-                    this.$set(this._cq, 'breakpoints', this.$options.cq);
-                }
-            },
-            mounted() {
-                if (goodToGo(this)) {
-                    Object.keys(this._cq.breakpoints).forEach((breakpoint) => {
-                        this._cq[breakpoint] = false;
-                    });
-
-                    this._cq.resizeObserver = new ResizeObserver((entries) => {
-                        console.log({ entries });
-                        entries.forEach((entry) => {
-                            this.$set(this._cq, 'contentRect', entry.contentRect.toJSON());
-                            Object.keys(this._cq.breakpoints).forEach((breakpoint) => {
-                                const rules = this._cq.breakpoints[breakpoint];
-                                Object.entries(rules).forEach((rule) => {
-                                    const cssMqRule = rule[0];
-                                    const cssMqValue = rule[1];
-                                    const fieldToTest = (cssMqRule.toLowerCase().includes('width')) ? 'width' : 'height';
-                                    this.$set(this._cq, breakpoint, cssMediaQueryResolver[cssMqRule](cssMqValue, this._cq.contentRect[fieldToTest]));
-                                    this.$emit('$cq:resize', this._cq.contentRect);
-                                });
-                            });
-                        });
-                    });
-                    this._cq.resizeObserver.observe(this.$el);
-                }
-            },
-            destroyed() {
-                if (goodToGo(this)) {
-                    this._cq.resizeObserver.disconnect();
-                }
-            }
-        });
-    }
+		Vue.mixin(getMixin(normalizedOptions));
+	}
 };
+
+
+export const VueContainerQueryMixin = (options = {}) => {
+	optionsUtils.validate(options, defaultOptions);
+	const normalizedOptions = optionsUtils.normalize(options, defaultOptions);
+
+	return getMixin(normalizedOptions);
+}
